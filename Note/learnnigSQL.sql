e7c4296d0936efb40691e13f4925a2b55a66a1c7
nirdongssh-keygen -t rsa -C 476189393@qq.com.ssh/id_rsa.pub/Users/Moli/.ssh/id_rsassh-keygen -t rsa -C 476189393@qq.comgit config --global user.name nirdong <476189393@qq.com>
2019-05-04 22:01:18 +0800

    First Commit

--- Note/learnnigSQL.sql
+++ Note/learnnigSQL.sql
@@ -4,3 +4,14 @@ show databases;
 use Northwind;
 show tables;
 select * from Orders;
+
+
+SELECT DISTINCT Company FROM Orders 
+
+SELECT * FROM Persons WHERE City='Beijing'
+
+SELECT * FROM Persons WHERE FirstName='Bush'
+
+SELECT * FROM Persons WHERE Year>1965
+
+SELECT * FROM Persons WHERE FirstName='Thomas' AND LastName='Carter'
