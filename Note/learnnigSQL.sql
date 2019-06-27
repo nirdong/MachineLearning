f26e3c3db4ed06f9d390bee9aae64fcd28ab310f
nirdongssh-keygen -t rsa -C 476189393@qq.com.ssh/id_rsa.pub/Users/Moli/.ssh/id_rsassh-keygen -t rsa -C 476189393@qq.comgit config --global user.name nirdong <476189393@qq.com>
2019-06-27 17:26:47 +0800

    1

--- Note/learnnigSQL.sql
+++ Note/learnnigSQL.sql
@@ -15,3 +15,9 @@ SELECT * FROM Persons WHERE FirstName='Bush'
 SELECT * FROM Persons WHERE Year>1965
 
 SELECT * FROM Persons WHERE FirstName='Thomas' AND LastName='Carter'
+
+SELECT * FROM Persons WHERE firstname='Thomas' OR lastname='Carter'
+
+SELECT Company, OrderNumber FROM Orders ORDER BY Company
+
+SELECT Company, OrderNumber FROM Orders ORDER BY Company DESC
